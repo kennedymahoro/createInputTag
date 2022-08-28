6493a134939e257c01c632ae5108864bfab2cb92
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-08-27 20:26:01 -0400

    initial commit

--- testmyapp/src/App.jsx
+++ testmyapp/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
@@ -17,9 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1>Hello world!</h1>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
