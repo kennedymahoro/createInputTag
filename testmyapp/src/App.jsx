8f4ba4cba81ac6b94f03b4411aaa21d56bcb6a0f
Kennedy Mahoro <106330299+kennedymahoro@users.noreply.github.com>
2022-08-29 16:50:28 -0400

    still has some issues, but I have made progress.

--- testmyapp/src/App.jsx
+++ testmyapp/src/App.jsx
@@ -1,29 +1,8 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
-
+import InputTag from "./Components/InputTag.jsx"
 function App() {
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h1>Hello world!</h1>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <InputTag />
   )
 }
 
---
+++ testmyapp/src/Components/InputTag.jsx
@@ -0,0 +1,19 @@
+import {useState} from "react"
+
+
+const InputTag = () => {
+  const [name, setName] = useState();
+  const handleSubmit = (event) => {
+    event.preventDefualt()
+    alert(`The name you Entered was: ${name}`)
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Enter a name:      
+        <input type='text' value={name} onChange{(e)=>{setName(e.target.value)}}/>
+      </label>
+      <input type='submit' />
+    </form>
+  )
+}
+export default InputTag;
